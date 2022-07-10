25d0b3d25ddec7463e382feca01d1c4c56f5c994
Denis.Viskov <Denis.Viskov@nordigy.ru>
2022-07-10 17:34:32 +0300

    commit

--- src/com/company/dejkstraSearch/DijkstraSearcher.java
+++ src/com/company/dejkstraSearch/DijkstraSearcher.java
@@ -1,48 +1,43 @@
 package com.company.dejkstraSearch;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DijkstraSearcher {
 
-    private static final Set<Nodes> PROCESSED = new HashSet<>();
-
+    /**
+     * Run by graph -> updates costs of neighbors and put them to map
+     * Find the lowest cost node amid of current neighbors
+     * Put it to map by current node in for loop
+     *
+     * @param graph
+     * @return path
+     */
     public static Map<Nodes, Nodes> search(
-        final Map<Nodes, Map<Nodes, Long>> graph,
-        final Map<Nodes, Long> costs
-    ) {
-        var parents = initParents(graph);
-
-        var node = findLowestCostNode(costs);
-        while (node != null) {
-            final var cost = costs.get(node);
-
-            final var neighbors = graph.get(node);
-            for (final var n : neighbors.keySet()) {
-                final var newCost = cost + neighbors.get(n);
-                if (costs.get(n) > newCost) {
-                    costs.put(n, newCost);
-                    parents.put(n, node);
-                }
-            }
-            PROCESSED.add(node);
-            node = findLowestCostNode(costs);
-        }
-        return resultParents(parents);
-    }
-
-    private static Map<Nodes, Nodes> initParents(
         final Map<Nodes, Map<Nodes, Long>> graph
     ) {
-        final var parents = new HashMap<Nodes, Nodes>();
-        graph.forEach((it, neighbors) ->
-            neighbors.keySet().forEach(neighbor -> parents.put(neighbor, it))
-        );
-        return parents;
+        final var map = new HashMap<Nodes, Nodes>();
+        final var costs = initCosts(graph);
+
+        graph.forEach((it, neighbors) -> {
+            neighbors.forEach((neighbor, cost) -> {
+                final var newCost = costs.get(it) + cost;
+                if (newCost < costs.get(neighbor)) {
+                    costs.put(neighbor, newCost);
+                    map.put(neighbor, it);
+                }
+            });
+            final var lowestCostNode = findLowestCostNode(neighbors);
+            map.put(lowestCostNode, it);
+        });
+        return resultParents(map);
     }
 
+    /**
+     * Build path from END to START node
+     *
+     * @param parents
+     * @return path
+     */
     private static Map<Nodes, Nodes> resultParents(final Map<Nodes, Nodes> parents) {
         final var result = new HashMap<Nodes, Nodes>();
         Nodes node = parents.get(Nodes.END);
@@ -55,17 +50,48 @@ public class DijkstraSearcher {
         return result;
     }
 
-    private static Nodes findLowestCostNode(final Map<Nodes, Long> costs) {
-        var lowestCost = Long.MAX_VALUE;
+    /**
+     * Init costs where START node is 0 and others infinity
+     *
+     * @param graph
+     * @return costs
+     */
+    private static Map<Nodes, Long> initCosts(
+        final Map<Nodes, Map<Nodes, Long>> graph
+    ) {
+        final var costs = new HashMap<Nodes, Long>();
+        costs.put(Nodes.START, 0L);
+        graph.keySet().forEach(it -> {
+            if (it != Nodes.START) {
+                costs.put(it, Long.MAX_VALUE);
+            }
+        });
+        return costs;
+    }
 
-        Nodes lowestCostNode = null;
-        for (final var node : costs.keySet()) {
-            final var cost = costs.get(node);
-            if (cost < lowestCost && !PROCESSED.contains(node)) {
-                lowestCost = cost;
-                lowestCostNode = node;
+    /**
+     * Find the lowest cost node
+     *
+     * @param neighbors
+     * @return Nodes
+     */
+    private static Nodes findLowestCostNode(
+        final Map<Nodes, Long> neighbors
+    ) {
+        Nodes result = null;
+        Long cost = null;
+        for (final var entry : neighbors.entrySet()) {
+            if (result == null) {
+                result = entry.getKey();
+            }
+            if (cost == null) {
+                cost = entry.getValue();
+            }
+            if (entry.getValue() < cost) {
+                cost = entry.getValue();
+                result = entry.getKey();
             }
         }
-        return lowestCostNode;
+        return result;
     }
 }
