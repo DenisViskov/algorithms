aecc17b6439b1e7a1f81772839e9e7b0238fda10
denis <denisviskov16@gmail.com>
2022-07-19 14:05:07 +0400

    some

--- src/com/company/dynamicProgramming/AttractionSolution.java
+++ src/com/company/dynamicProgramming/AttractionSolution.java
@@ -3,14 +3,22 @@ package com.company.dynamicProgramming;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public class AttractionSolution {
 
-    private final Map<Double, Integer> cellsByDays = Map.of(
+    private static final Map<Double, Integer> CELLS_BY_DAYS = Map.of(
             0.5, 0,
             1.0, 1,
             1.5, 2,
             2.0, 3
+    );
+
+    private static final Map<Integer, Double> DAYS_BY_CELLS = Map.of(
+            0, 0.5,
+            1, 1.0,
+            2, 1.5,
+            3, 2.0
     );
 
     /**
@@ -28,8 +36,49 @@ public class AttractionSolution {
      * @return
      */
     public static List<Attraction> selectedAttractions(final List<Attraction> attractions) {
-        final var table = new int[attractions.size()][4];
+        final var table = new int[attractions.size()][CELLS_BY_DAYS.size()];
+
+        for (int attrIndex = 0; attrIndex < table.length; attrIndex++) {
+            final var attraction = attractions.get(attrIndex);
+            for (int daysIndex = 0; daysIndex < table[attrIndex].length; daysIndex++) {
+                if (attrIndex > 0) {
+                    setWhenHasPreviousAttraction(attraction, table, attrIndex, daysIndex);
+                } else {
+                    setWhenPreviousAttractionAbsent(attraction, table, attrIndex, daysIndex);
+                }
+            }
+        }
 
         return Collections.emptyList();
+    }
+
+    private static void setWhenPreviousAttractionAbsent(
+            final Attraction attraction,
+            final int[][] table,
+            final int attrIndex,
+            final int daysIndex
+    ) {
+        if (attraction.getTimeInDays() <= DAYS_BY_CELLS.get(daysIndex)) {
+            table[attrIndex][daysIndex] = attraction.getGrade();
+        } else {
+            table[attrIndex][daysIndex] = 0;
+        }
+    }
+
+    private static void setWhenHasPreviousAttraction(
+            final Attraction attraction,
+            final int[][] table,
+            final int attrIndex,
+            final int daysIndex
+    ) {
+        if (attraction.getTimeInDays() < DAYS_BY_CELLS.get(daysIndex)) {
+            final var remainsFree = DAYS_BY_CELLS.get(daysIndex) - attraction.getTimeInDays();
+            final var summWithPrevMax = table[attrIndex - 1][CELLS_BY_DAYS.get(remainsFree)] + attraction.getGrade();
+            table[attrIndex][daysIndex] = Math.max(summWithPrevMax, table[attrIndex - 1][daysIndex]);
+        } else if (Objects.equals(attraction.getTimeInDays(), DAYS_BY_CELLS.get(daysIndex))) {
+            table[attrIndex][daysIndex] = Math.max(attraction.getGrade(), table[attrIndex - 1][daysIndex]);
+        } else {
+            table[attrIndex][daysIndex] = table[attrIndex - 1][daysIndex];
+        }
     }
 }
